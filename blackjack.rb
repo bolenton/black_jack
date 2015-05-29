c1543c8ec0667ee900190bbac4a800cdbef4f821
Taurus Omejia <bolenton@gmail.com>
2015-05-28 21:19:35 -0500

    added player name

--- blackjack.rb
+++ blackjack.rb
@@ -50,85 +50,93 @@ def display_total(player_total, dealer_total, name)
 end
 
 # Start Game 
-clear_screen
-design
-puts "Welcome to BlackJack! What is your name?"
-name = gets.chomp.capitalize
+play_again = ' '
 
-clear_screen
-# Deal cards
-player_hand = deal_player_hand(deck, name)
-if player_hand.inject(:+) == 21
-  puts "BlackJack! #{name} Win!" 
-end
-
-dealer_hand = deal_dealer_hand(deck)
-display_total(player_hand.inject(:+), dealer_hand.inject(:+), name)
-
- # Player Hits or Stands
-begin
-  player_total = player_hand.inject(:+)
-  if player_total == 21
-    puts "BlackJack! #{name} Win!"
-    exit
-  elsif player_total > 21
-    puts "#{name} has #{player_total}, #{name} BUSTED!"
-    exit
-end
-  design
-  puts "#{name} has #{player_hand.inject(:+)} total."
-  design
-  puts "Hit or Stand?"
-  player_choice = gets.chomp.downcase
-  if player_choice[0] == "h"
-    clear_screen
-    player_hand << hit(deck)
-    player_total = player_hand.inject(:+)
-    display_total(player_hand.inject(:+), dealer_hand.inject(:+), name)
-    design   
-  else
-    player_total = player_hand.inject(:+)
-  end
-end until player_choice[0] == "s"
-
-# Dealer hits or stand
-begin
+begin 
   clear_screen
-  display_total(player_hand.inject(:+), dealer_hand.inject(:+), name)
   design
-  puts "DEALER has #{dealer_hand.inject(:+)} total."  
-  design
-  
-  dealer_total = dealer_hand.inject(:+)
-  if dealer_total == 21
-    puts "Dealer has BlackJack. #{name} Lose"
-    break
-  elsif dealer_total > 21
-    puts "Dealer Bust! #{name} Win"
-    break
-  elsif dealer_total > 16
-    puts "Dealer stands at #{dealer_total}"
-    sleep(3)
-    dealer_stands = true
-  elsif dealer_total < 17
-    dealer_hand << hit(deck)
-    sleep(3)
-    next
-  end 
-end until dealer_stands
+  puts "Welcome to BlackJack! What is your name?"
+  name = gets.chomp.capitalize
+  clear_screen
 
-if dealer_stands
-  if dealer_total > player_total
-    design
-    puts "Dealer has #{dealer_total} and #{name} has #{player_total}"
-    puts "Dealer WON!"
-    design
-  elsif player_total > dealer_total
-    design
-    puts "#{name} has #{player_total}, and Dealer has #{dealer_total}"
-    puts "#{name} WON!"
-    design
-  else
-    puts "Its a tie!"
+  # Deal cards
+  player_hand = deal_player_hand(deck, name)
+  if player_hand.inject(:+) == 21
+    puts "BlackJack! #{name} Win!" 
   end
-end
+
+  dealer_hand = deal_dealer_hand(deck)
+  display_total(player_hand.inject(:+), dealer_hand.inject(:+), name)
+
+   # Player Hits or Stands
+  begin
+    player_total = player_hand.inject(:+)
+    if player_total == 21
+      puts "BlackJack! #{name} Win!"
+      exit
+    elsif player_total > 21
+      puts "#{name} has #{player_total}, #{name} BUSTED!"
+      exit
+  end
+    design
+    puts "#{name} has #{player_hand.inject(:+)} total."
+    design
+    puts "Hit or Stand?"
+    player_choice = gets.chomp.downcase
+    if player_choice[0] == "h"
+      clear_screen
+      player_hand << hit(deck)
+      player_total = player_hand.inject(:+)
+      display_total(player_hand.inject(:+), dealer_hand.inject(:+), name)
+      design   
+    else
+      player_total = player_hand.inject(:+)
+    end
+  end until player_choice[0] == "s"
+
+  # Dealer hits or stand
+  begin
+    clear_screen
+    display_total(player_hand.inject(:+), dealer_hand.inject(:+), name)
+    design
+    puts "DEALER has #{dealer_hand.inject(:+)} total."  
+    design
+    
+    dealer_total = dealer_hand.inject(:+)
+    if dealer_total == 21
+      puts "Dealer has BlackJack. #{name} Lose"
+      break
+    elsif dealer_total > 21
+      puts "Dealer Bust! #{name} Win"
+      break
+    elsif dealer_total > 16
+      puts "Dealer stands at #{dealer_total}"
+      sleep(3)
+      dealer_stands = true
+    elsif dealer_total < 17
+      dealer_hand << hit(deck)
+      sleep(3)
+      next
+    end 
+  end until dealer_stands
+
+  if dealer_stands
+    if dealer_total > player_total
+      design
+      puts "Dealer has #{dealer_total} and #{name} has #{player_total}"
+      puts "Dealer WON!"
+      design
+    elsif player_total > dealer_total
+      design
+      puts "#{name} has #{player_total}, and Dealer has #{dealer_total}"
+      puts "#{name} WON!"
+      design
+    else
+      puts "Its a tie!"
+    end
+  end
+
+  puts "Play again? Y/N?"
+  play_again = gets.chomp.downcase
+
+end until play_again[0] == 'n'
